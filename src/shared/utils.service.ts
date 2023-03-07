4b7b727b4e388c5249082bfb4c0e5077b3d432ab
Luis Lovera <luislovera87@gmail.com>
2023-03-06 21:24:08 -0600

    removing unused method

--- src/shared/utils.service.ts
+++ src/shared/utils.service.ts
@@ -1,10 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { customAlphabet } from 'nanoid'
 
 @Injectable()
 export class UtilsService {
-    generateId() {
-        const nanoid = customAlphabet('1234567890abcdef', 10)
-        return nanoid(7);
-    }
 }
